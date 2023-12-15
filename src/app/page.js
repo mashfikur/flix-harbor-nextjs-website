09e7c2cc675f85eb40bc63b2ed785ea826edb3c1
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-16 01:50:20 +0600

    dynamic image fethced from api

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar/navbar";
+import { Button } from "@mui/material";
+import Banner from "./components/Banner/banner";
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-4xl text-center' >Hello</h1>
+      <div className="container border-b-2 border-amber-600 mx-auto py-8 flex justify-between items-center">
+        <div className=""><h3 className="text-5xl font-bold"> Flix Harbor</h3></div>
+        <div className=" text-end" >
+          <ul className="flex items-center gap-10 font-semibold">
+            <li>Home</li>
+            <li>Movie</li>
+            <li>TV Show</li>
+            <li>Account</li>
+          </ul>
+        </div>
+    
+      </div>
+
+      {/* banner */}
+      <Banner></Banner>
     </div>
   );
 };
