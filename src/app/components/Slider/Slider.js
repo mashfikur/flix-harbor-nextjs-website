162de92d5f61205ecf57bdf8a0911cec31de70a2
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-12-17 21:31:07 +0600

    dynamic slide data loaded in website

--- src/app/components/Slider/Slider.js
+++ src/app/components/Slider/Slider.js
@@ -1,75 +1,75 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import "./styles.css"
+import "./styles.css";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/effect-cards";
 
 // import required modules
-import { EffectCards } from "swiper/modules";
+import { EffectCards, Autoplay } from "swiper/modules";
 import Image from "next/image";
+import { duration } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
-export default function App() {
+//eslint-disable-next-line
+export default function Slider() {
   const imgUrl = `https://image.tmdb.org/t/p/original/thLAoL6VeZGmCyDpCOeoxLvA8yS.jpg`;
-  const img2 =
-    "https://image.tmdb.org/t/p/original/dd7EgfOEKPqQxWtBfAvjYZahbSc.jpg";
 
-  const img3 =
-    "https://image.tmdb.org/t/p/original//kteLc9xtCOOjwvVNHjxNqJxu0OA.jpg";
-  const img4 =
-    "https://image.tmdb.org/t/p/original/7Ns6tO3aYjppI5bFhyYZurOYGBT.jpg";
+  // const { data: currents } = useQuery({
+  //   queryKey: ["currents"],
+  //   queryFn: async () => {
+  //     const res = await axios.get(
+  //       "https://api.themoviedb.org/3/trending/all/day?language=en-US",
+  //       {
+  //         headers: {
+  //           Accept: "application/json",
+  //           Authorization:
+  //             "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMWMzMTkyNjU0NGQzMmNiNjljZDk2MDY3MDk3NTQ5ZSIsInN1YiI6IjY1N2FlZGYxZWM4YTQzMDBlMDliNWUyYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U2kr6J-oqi3Wl93bR0RpCcErIuA9S-USv47_XDjgZPI",
+  //         },
+  //       }
+  //     );
+
+  //     return res.data;
+  //   },
+  // });
+
+  // console.log(currents);
+
   return (
-    <div className="container mx-auto">
-      <Swiper
-        effect={"cards"}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper w-80 mx-auto"
-        loop={true}
-      >
-        <SwiperSlide>
-          {" "}
-          <Image
-            alt="slider-image"
-            width={500}
-            height={400}
-            src={imgUrl}
-            className=""
-          ></Image>{" "}
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <Image
-            alt="slider-image"
-            width={500}
-            height={400}
-            src={img2}
-            className=""
-          ></Image>{" "}
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <Image
-            alt="slider-image"
-            width={500}
-            height={400}
-            src={img3}
-            className=""
-          ></Image>{" "}
-        </SwiperSlide>
-        <SwiperSlide>
-          {" "}
-          <Image
-            alt="slider-image"
-            width={500}
-            height={400}
-            src={img4}
-            className=""
-          ></Image>{" "}
-        </SwiperSlide>
-      </Swiper>
+    <div className="container mx-auto my-32 flex items-center ">
+      <div className="flex-1">
+        <h3 className="text-7xl text-amber-600 font-semibold">
+          Pick Your Content Of The Day
+        </h3>
+      </div>
+      <div className="flex-1">
+        <Swiper
+          effect={"cards"}
+          grabCursor={true}
+          modules={[EffectCards]}
+          className="mySwiper w-80 mx-auto"
+          loop={true}
+          autoplay={{ duration: 2500 }}
+        >
+          {/* {movies &&
+            movies?.results.map((moive, idx) => (
+              <SwiperSlide key={idx}>
+                {" "}
+                <Image
+                  alt="slider-image"
+                  width={500}
+                  height={400}
+                  src={imgUrl}
+                  className=""
+                ></Image>{" "}
+              </SwiperSlide>
+            ))} */}
+        </Swiper>
+      </div>
     </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -8,10 +9,15 @@ export const metadata = {
   description: "A movie and tv shows collection website",
 };
 
+// const queryClient = new QueryClient();
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="light" >
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="light">
+      <body className={inter.className}>
+        {/* <QueryClientProvider client={queryClient}></QueryClientProvider> */}
+        {children}
+      </body>
     </html>
   );
 }
